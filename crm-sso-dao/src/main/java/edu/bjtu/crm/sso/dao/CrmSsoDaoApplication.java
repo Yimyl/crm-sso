3f0a3aee1c264f26974deaa2a0164f45fb52b4bf
zhaozhengjian <yimyl847274669@gmail.com>
2020-01-11 16:59:48 +0800

    dao mudule

---
+++ crm-sso-dao/src/main/java/edu/bjtu/crm/sso/dao/CrmSsoDaoApplication.java
@@ -0,0 +1,13 @@
+package edu.bjtu.crm.sso.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSsoDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmSsoDaoApplication.class, args);
+    }
+
+}
---
+++ crm-sso-dao/src/test/java/edu/bjtu/crm/sso/dao/CrmSsoDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bjtu.crm.sso.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSsoDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
